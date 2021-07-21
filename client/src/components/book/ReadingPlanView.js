3eca29a4f0bf745ecce5fb8ddd38d0110bbe5a4b
lwlsimm <77008736+lwlsimm@users.noreply.github.com>
2021-07-21 23:47:36 +0100

    redo readingplan

--- client/src/components/book/ReadingPlanView.js
+++ client/src/components/book/ReadingPlanView.js
@@ -10,10 +10,6 @@ import { updateExistingPlan } from '../../functions/readingPlanFunctions';
 import { recalculate_plan } from '../../functions/recalculatePlanFunction'
 import { useHistory } from 'react-router-dom';
 
-function useForceUpdate(){
-  const [value, setValue] = useState(0); // integer state
-  return () => setValue(value => value + 1); // update the state to force render
-}
 
 function ReadingPlanView () {
   
@@ -22,7 +18,7 @@ function ReadingPlanView () {
   const [book_data,setBook_data] = useState({});
   const [scheme, setScheme] = useState([]);
   const queryParams = window.location.search.substring(1);
-  const forceUpdate = useForceUpdate();
+  
   
   useEffect(()=> {
     //setting the plan from the plans in the state
@@ -41,9 +37,30 @@ function ReadingPlanView () {
     return schemeArray;
   }
 
-  function updateScheme(index, item, value) {
+  function updateScheme(index, category, value) {
     const newScheme = [...scheme];
-    newScheme[index][item] = value;
+    newScheme[index][category] = value;
+    //check subsequent dates are still chronological
+    for(let i = index + 1; i < newScheme.length;i++){
+      const today = new Date(newScheme[i]['date']);
+      const yesterday = new Date(newScheme[i-1]['date']);
+      if(today <= yesterday) {
+        const newDateForToday = new Date(newScheme[i-1]['date']);
+        newDateForToday.setDate(newDateForToday.getDate() + 1);
+        newScheme[i]['date'] = newDateForToday;
+      }
+    }
+    setScheme(newScheme);
+  }
+
+  function findPreviousDayTo(index) {
+
+  }
+
+  function noReadingForToday(index) {
+    const newScheme = [...scheme];
+    newScheme[index]['to'] = 'None';
+    newScheme[index]['from'] = 'None';
     setScheme(newScheme);
   }
 
@@ -83,16 +100,17 @@ function ReadingPlanView () {
             const from = item.from;
             const to = item.to;
             const status = item.completed? 'Read':'Unread';
+            const previousDayTo = 0;
             
             return(
             <Fragment>
-              <div className="RP-col1">{item.day}</div>
+              <div className="RP-col1">{day}</div>
               <input type="date" className="RP-col2" value={formatDate(today)} onChange={(e)=>updateScheme(index,"date",e.target.value)}/>
               <input className="RP-col3" defaultValue={from} onChange={(e)=>updateScheme(index,"from",Number(e.target.value))}/>
               <input className="RP-col4" defaultValue={to} onChange={(e)=>updateScheme(index,"to",Number(e.target.value))}/>
               <div className="RP-col5">{status}</div>
               <img className="RP-col6 RP-icon" src={furthestReadIcon} alt="furthest read to"/>
-              <img className="RP-col7 RP-icon" src={noReading} alt="no reading today"/>
+              <img className="RP-col7 RP-icon" src={noReading} alt="no reading today" onClick={()=>noReadingForToday(index)}/>
             </Fragment>)
           })
         }
